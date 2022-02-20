7f63b9dcbe40a718112605e6e1ac6ec29c1d26f7
Talvinder Singh <talvindersingh1992@gmail.com>
2022-02-20 22:31:13 +0530

    added app

---
+++ app.js
@@ -0,0 +1,9 @@
+const formtojson = (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  let object = {};
+  formData.forEach((value, key) => (object[key] = value));
+  let json = JSON.stringify(object);
+  return json;
+};
+export default formtojson;
